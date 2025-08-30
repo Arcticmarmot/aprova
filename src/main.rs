e290906852356495934833172c2ce45b5a8a661c
marmot <arctic_marmot@outlook.com>
2025-08-30 22:13:06 +0800

    feat: 创建复数类

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+use std::ops::Add;
+
+#[derive(Clone, Copy, Debug)]
+struct Complex<T: Add<Output=T> + Copy> {
+    re: T,
+    im: T,
+}
+
+impl<T: Add<Output=T> + Copy> Add for Complex<T> {
+    type Output = Complex<T>;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        Complex {
+            re: self.re + rhs.re,
+            im: self.im + rhs.im
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let c1 = Complex::<u32> { re: 12, im: 12 };
+    let c2 = Complex::<u32> { re: 32, im: 1 };
+    println!("{:?}", c1 + c2);
+    println!("{:?}", c1);
 }
